d3efd6fd66d2d3ae33e7ef026274b840bb7ff891
Prem Shankar Kumar <premshankar847232@gmail.com>
2020-10-06 11:31:39 +0530

    Express simple rest

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const hostname='localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+app.all('/dishes', (req,res,next)=> {
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+
+});
+
+app.get('/dishes',(req,res,next)=>{
+    res.end('will send all the dishes');
+});
+app.post('/dishes',(req,res,next)=>{
+    res.end('Will add the dish: '+req.body.name+'with details:'+ req.body.description);
+
+});
+app.put('/dishes',(req,res,next)=> {
+    res.statusCode=403;
+    res.end("this operation doesn't make any sense");
+});
+app.delete('/dishes',(req,res,next)=>{
+    res.end("Deleting all the dishes");
+});
+app.get('/dishes/:dishId',(req,res,next)=>{
+    res.end('will send details of the dish: '+req.params.dishId+" to you");
+});
+app.post('/dishes/:dishId',(req,res,next)=>{
+    res.end('Post not supported for dishes for dish id: '+req.params.dishId);
+
+});
+app.put('/dishes/:dishId',(req,res,next)=> {
+    res.write('Updating the dish '+req.params.dishId);
+    res.end('will update the dish: '+req.body.name
+    +'with details: '+req.body.description);
+    res.end("this operation doesn't make any sense");
+});
+app.delete('/dishes/:dishId',(req,res,next)=>{
+    res.end("Deleting all the dishes");
+});
 app.use(express.static(__dirname + '/public'));
 app.use((req,res, next) =>{
     console.log(req.headers);
